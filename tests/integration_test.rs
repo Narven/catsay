495968c4e972557f5c135a824dd96cb76947e64c
Pedro Luz <pedromsluz@gmail.com>
2022-04-17 00:35:42 +0100

    added more integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,4 +1,5 @@
 use assert_cmd::prelude::*;
+use predicates::prelude::*;
 use std::process::Command;
 
 #[test]
@@ -6,6 +7,17 @@ fn run_with_defaults() -> Result<(), Box<dyn std::error::Error>> {
     Command::cargo_bin("catsay")
         .expect("binary exists")
         .assert()
-        .success();
+        .success()
+        .stdout(predicate::str::contains("Meow!"));
+    Ok(())
+}
+
+#[test]
+fn run_on_non_existing_file() -> Result<(), Box<dyn std::error::Error>> {
+    Command::cargo_bin("catsay")
+        .expect("binary exists")
+        .args(&["-f", "no/such/file.txt"])
+        .assert()
+        .failure();
     Ok(())
 }
